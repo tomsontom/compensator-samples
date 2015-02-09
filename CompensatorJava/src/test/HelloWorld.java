6a2ea9a2a23486ab0a3df6ca1eae7cc70327a9a5
Tom Schindl <tom.schindl@bestsolution.at>
2015-02-10 00:15:24 +0100

    A sample message Tue Feb 10 00:15:24 CET 2015

--- CompensatorJava/src/test/HelloWorld.java
+++ CompensatorJava/src/test/HelloWorld.java
@@ -1,7 +1,7 @@
 package test;
 
-public class HelloWorld {
+public class HelloWorld {	public static double div(int a, int b) {		return a / b;    }    
 	public static void main(String[] args) {
-		System.out.println("Hello World");		// TODO Test Me
+		System.out.println("Hello World");		int a = 1;		System.out.println("The new value is " + a);
 	}
 }
