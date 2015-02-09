aabfb87241509cc9e216794f185eb2e3d56bab4a
Tom Schindl <tom.schindl@bestsolution.at>
2015-02-10 00:21:03 +0100

    added !
    commit for #1

--- CompensatorJava/src/test/HelloWorld.java
+++ CompensatorJava/src/test/HelloWorld.java
@@ -2,6 +2,6 @@ package test;
 
 public class HelloWorld {	public static double div(int a, int b) {		return a / b;    }    
 	public static void main(String[] args) {
-		System.out.println("Hello World");		int a = 1;		System.out.println("The new value is " + a);
+		System.out.println("Hello World!");		int a = 1;		System.out.println("The new value is " + a);
 	}
 }
