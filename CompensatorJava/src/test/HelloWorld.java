885ee354cccae4134cd7fe90d964523d5a5474f4
Tom Schindl <tom.schindl@bestsolution.at>
2015-02-10 00:22:12 +0100

    fixed div
    commit for #1

--- CompensatorJava/src/test/HelloWorld.java
+++ CompensatorJava/src/test/HelloWorld.java
@@ -1,6 +1,6 @@
 package test;
 
-public class HelloWorld {	public static double div(int a, int b) {		return a / b;    }    
+public class HelloWorld {	public static double div(int a, int b) {		return a * 1.0 / b;    }    
 	public static void main(String[] args) {
 		System.out.println("Hello World!");		int a = 1;		System.out.println("The new value is " + a);
 	}
