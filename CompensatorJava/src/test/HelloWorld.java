ca1f5db42d43855ab9b9b0603ae01ae32acd400f
Tom Schindl <tom.schindl@bestsolution.at>
2015-02-17 00:57:07 +0100

    adding division sample
    commit for #1

--- CompensatorJava/src/test/HelloWorld.java
+++ CompensatorJava/src/test/HelloWorld.java
@@ -1,7 +1,7 @@
 package test;
 
-public class HelloWorld {	public static double div(int a, int b) {		return a / b;    }    
+public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");		int a = 1;		System.out.println("The new value is " + a);
+		System.out.println("10/3" + MathHelper.div(10,3));
 	}
 }
