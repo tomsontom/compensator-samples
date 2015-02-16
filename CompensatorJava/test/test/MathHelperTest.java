d9726e57adcef6fa6bc392cc638b35dccc55c43f
Tom Schindl <tom.schindl@bestsolution.at>
2015-02-16 16:34:26 +0100

    #2 - push adding test for div

--- CompensatorJava/test/test/MathHelperTest.java
+++ CompensatorJava/test/test/MathHelperTest.java
@@ -6,5 +6,6 @@ public class MathHelperTest {
 	@Test
 	public void testDiv() {
 		org.junit.Assert.assertEquals(10.0/5.0,MathHelper.div(10,5),0.1);
+		org.junit.Assert.assertEquals(10.0/3.0,MathHelper.div(10,3),0.1);
 	}
 }
