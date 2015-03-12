2310b765990030fc5d26a70b001e13419bbce5a1
Tom Schindl <tom.schindl@bestsolution.at>
2015-03-11 21:51:38 -0700

    commit for #2

--- CompensatorJava/test/test/MathHelperTest.java
+++ CompensatorJava/test/test/MathHelperTest.java
@@ -5,6 +5,6 @@ import org.junit.Test;import static org.junit.Assert.*;
 public class MathHelperTest {
 	@Test
 	public void testDiv() {
-		assertEquals(10.0/5.0,MathHelper.div(10,5),0.1);
+		assertEquals(10.0/5.0,MathHelper.div(10,5),0.1);		assertEquals(10.0/3.0,MathHelper.div(10,3),0.1);
 	}
 }
